74b567960b7893de674a12126190e2ad5ec4cece
officemendante-art <officemendante@gmail.com>
2026-06-13 00:29:23 +0530

    Delete app directory

--- app/src/main/java/com/example/core/navigation/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.core.navigation
-
-sealed class Screen(val route: String) {
-    object Calculator : Screen("calculator")
-    object CurrencyCounter : Screen("currency_counter")
-}
